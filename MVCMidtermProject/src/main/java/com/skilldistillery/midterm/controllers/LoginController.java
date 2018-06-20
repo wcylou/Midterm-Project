46d189cd557eb3b8a7add4828d5492ee4e6d9cc1
Wilson <wilson_lou@hotmail.co.uk>
2018-06-20 16:45:37 -0600

    stubbed MVC

---
+++ MVCMidtermProject/src/main/java/com/skilldistillery/midterm/controllers/LoginController.java
@@ -0,0 +1,21 @@
+package com.skilldistillery.midterm.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import com.skilldistillery.midterm.data.EventDAO;
+import com.skilldistillery.midterm.data.MatchDAO;
+import com.skilldistillery.midterm.data.UserDAO;
+
+@Controller
+public class LoginController {
+	
+	@Autowired
+	private EventDAO edao;
+	@Autowired
+	private MatchDAO mdao;
+	@Autowired
+	private UserDAO udao;
+
+
+}
