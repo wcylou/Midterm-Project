120ca78235145bd16d998efd5a75bc2e02350784
JRBotta <jeremy.r.botta@gmail.com>
2018-06-21 14:33:30 -0600

    updated matchDAO

--- MVCMidtermProject/src/main/java/com/skilldistillery/midterm/data/MatchDAOImpl.java
+++ MVCMidtermProject/src/main/java/com/skilldistillery/midterm/data/MatchDAOImpl.java
@@ -17,40 +17,60 @@ import com.skilldistillery.midterm.entities.Profile;
 @Component
 public class MatchDAOImpl implements MatchDAO {
 
-	 @PersistenceContext
-	  private EntityManager em;
-	 
-		public Match findEventMatch(Profile profile, Profile partner) {
-			List<Interest> common = new ArrayList<>();
-			List<Interest> profileInterests = profile.getInterests();
-			List<Interest> partnerInterests = partner.getInterests();
-			
-			for (Interest interest : profileInterests) {
-				if(partnerInterests.contains(interest)) {
-					common.add(interest);
-				}
+	@PersistenceContext
+	private EntityManager em;
+
+	public Match findEventMatch(Profile profile, Profile partner) {
+		List<Interest> common = new ArrayList<>();
+		List<Interest> profileInterests = profile.getInterests();
+		List<Interest> partnerInterests = partner.getInterests();
+
+		for (Interest interest : profileInterests) {
+			if (partnerInterests.contains(interest)) {
+				common.add(interest);
 			}
-			
-			if(common.size() == 0) {
-				common.add(em.find(Interest.class, 1));
-			}
-			return null;
 		}
-		
-		public List<Profile> findPotentialMatches(Profile profile) {
-			List<Interest> profileInterests = profile.getInterests();
-			List<List<Interest>> common = new ArrayList<>();
-			
-			String query = "SELECT p FROM Profile p WHERE p.id != :id";
-			List<Profile> partners = em.createQuery(query, Profile.class).setParameter("id", profile.getId()).getResultList();
-			
+
+		if (common.size() == 0) {
+			common.add(em.find(Interest.class, 1));
+		}
+		return null;
+	}
+
+	public List<Profile> findPotentialMatches(Profile profile) {
+		List<Interest> profileInterests = profile.getInterests();
+		List<List<Interest>> common = new ArrayList<>();
+
+		String query = "SELECT p FROM Profile p WHERE p.id != :id AND p.age > :min AND p.age < :max";
+		List<Profile> partners = em.createQuery(query, Profile.class).setParameter("id", profile.getId())
+				.setParameter("min", profile.getMinAge()).setParameter("max", profile.getMaxAge()).getResultList();
+		if (partners.size() != 0) {
 			for (int i = 0; i < partners.size(); i++) {
 				common.add(new ArrayList<>());
-				for (int j = 0; j < partners.get(i).getInterests().size(); j++) {
-					if(profileInterests.contains(interest)) {
-						
+				List<Interest> partnerInterests = partners.get(i).getInterests();
+				for (int j = 0; j < partnerInterests.size(); j++) {
+					if (profileInterests.contains(partnerInterests.get(j))) {
+						common.get(i).add(partnerInterests.get((j)));
 					}
 				}
 			}
+
+			List<Profile> result = new ArrayList<>();
+			for (int i = 0; i < 5; i++) {
+				int max = common.get(0).size();
+				int place = 0;
+				for (int j = 1; j < common.size(); j++) {
+					if (common.get(j).size() > max) {
+						max = common.get(j).size();
+						place = j;
+					}
+				}
+				result.add(partners.get(place));
+				common.remove(place);
+			}
+			return result;
 		}
+		return new ArrayList<>();
+	}
+
 }
