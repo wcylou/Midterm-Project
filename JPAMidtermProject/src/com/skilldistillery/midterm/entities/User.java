4ed7deec0e4c07f46c41ec52f031e266e3d83182
JRBotta <jeremy.r.botta@gmail.com>
2018-06-22 10:37:06 -0600

    fixed match thing

--- JPAMidtermProject/src/com/skilldistillery/midterm/entities/User.java
+++ JPAMidtermProject/src/com/skilldistillery/midterm/entities/User.java
@@ -6,9 +6,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.validation.constraints.NotNull;
-
-import org.hibernate.validator.constraints.Email;
 
 @Entity
 public class User {
