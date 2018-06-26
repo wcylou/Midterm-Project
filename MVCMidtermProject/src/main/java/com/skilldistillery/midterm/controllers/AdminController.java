6669dd71902a573fb2613e17557b103664672817
Tom Broderick <tombroderick@gmail.com>
2018-06-26 15:33:12 -0600

    updated profile

--- MVCMidtermProject/src/main/java/com/skilldistillery/midterm/controllers/AdminController.java
+++ MVCMidtermProject/src/main/java/com/skilldistillery/midterm/controllers/AdminController.java
@@ -210,15 +210,6 @@ public class AdminController {
 		return mv;
 	}
 	
-	@RequestMapping(path = "deactivateProfile.do", method = RequestMethod.GET)
-	public ModelAndView deactivateProfile(@RequestParam("id") int profileId) {
-		ModelAndView mv = new ModelAndView();
-		User u = udao.getUserFromProfileID(profileId);
-		udao.deleteUser(u);
-		List<Profile> profileList = udao.getAllProfiles();
-		mv.addObject("profileList", profileList);
-		mv.setViewName("WEB-INF/admin-profiles.jsp");
-		return mv;
-	}
+	
 	
 }
