72f85a656bafb74cf713ec725f3e8e628bf33b3f
Tom Broderick <tombroderick@gmail.com>
2018-06-27 11:03:19 -0600

    code cleanup

--- JPAMidtermProject/src/com/skilldistillery/midterm/entities/Event.java
+++ JPAMidtermProject/src/com/skilldistillery/midterm/entities/Event.java
@@ -16,78 +16,85 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
-
 @Entity
 public class Event {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private String name;
-	@ManyToOne(cascade= CascadeType.PERSIST)
-	@JoinColumn(name="location_id")
+	@ManyToOne(cascade = CascadeType.PERSIST)
+	@JoinColumn(name = "location_id")
 	private Location location;
 	@Temporal(TemporalType.DATE)
 	private Date date;
 	private String description;
-	@ManyToMany(cascade= {CascadeType.PERSIST, CascadeType.REMOVE})
+	@ManyToMany(cascade = { CascadeType.PERSIST, CascadeType.REMOVE })
 	@JoinTable(name = "event_interest", joinColumns = @JoinColumn(name = "event_id"), inverseJoinColumns = @JoinColumn(name = "interest_id"))
 	private List<Interest> interests;
-	
-	//gets and sets
+
+	// gets and sets
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
 	public Location getLocation() {
 		return location;
 	}
+
 	public void setLocation(Location location) {
 		this.location = location;
 	}
+
 	public Date getDate() {
 		return date;
 	}
+
 	public void setDate(Date date) {
 		this.date = date;
 	}
+
 	public String getDescription() {
 		return description;
 	}
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
+
 	public int getId() {
 		return id;
 	}
+
 	public List<Interest> getInterests() {
 		return interests;
 	}
+
 	public void setInterests(List<Interest> interests) {
 		this.interests = interests;
 	}
-	
+
 	public void addInterest(Interest interest) {
 		if (interests == null)
 			interests = new ArrayList<>();
-		
+
 		if (!interests.contains(interest)) {
 			interests.add(interest);
 			interest.addEvent(this);
 		}
-		
+
 	}
-	
+
 	public void removeInterest(Interest interest) {
 		if (interests != null && interests.contains(interest)) {
 			interests.remove(interest);
 			interest.removeEvent(this);
 		}
 	}
-	
-	
+
 	@Override
 	public String toString() {
 		StringBuilder builder = new StringBuilder();
@@ -102,6 +109,5 @@ public class Event {
 		builder.append("]");
 		return builder.toString();
 	}
-	
-	
+
 }
